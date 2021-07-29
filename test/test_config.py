b582f23c8b39cc426df6702d9d28e387b2d0b7e4
Jennifer Riordan <36043113+jenniferriordan@users.noreply.github.com>
2021-07-29 14:23:23 +0100

    tidy unit test

--- test/test_config.py
+++ test/test_config.py
@@ -652,15 +652,13 @@ class PipIndexConfigTest(TestCase):
         test_username = "foo@metoffice.gov.uk"
         test_password = "this_is_the_password"
 
-        sm_client = boto3.client("secretsmanager", region_name="eu-west-2")
-        sm_client.create_secret(Name=test_username, SecretString=test_password)
+        secrets_mgr: SecretsManagerClient = boto3.client("secretsmanager", region_name="eu-west-2")
+        secrets_mgr.create_secret(Name=test_username, SecretString=test_password)
 
         test_new_password = "new_password"
 
         pip_conf = PipIndexConfig(self.test_ssm_config, test_username, test_new_password)
         pip_conf.create_secrets(boto3.Session(region_name="eu-west-2"))
-
-        secrets_mgr: SecretsManagerClient = boto3.client("secretsmanager", region_name="eu-west-2")
 
         self.assertEqual(
             pip_conf.password,
